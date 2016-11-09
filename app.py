b0eeebe4f49a4ebbdf3619a0e7ae9783976ffb77
Sam Stultz <sks2200@columbia.edu>
2016-11-09 03:28:08 -0500

    signup works

--- app.py
+++ app.py
@@ -78,21 +78,23 @@ def validateLogin():
     return redirect('/SignUp')
 
 @app.route('/validateSignUp',methods=['POST'])
-def validateLogin():
+def validateSignUp():
     _username = request.form["username"]
     _password = request.form["password"]
     _age = request.form["age"]
-    _gender = request.form["gender"]
+    _gender = request.form.get("gender")
     cursor = g.conn.execute("select username from users where username='"+_username+"';")
+
     for result in cursor:
       if (_username == result['username']):
         flash("Username already taken, please choose another.", category='error')
         cursor.close()
         return redirect('/SignUp')
-    g.conn.execute("insert into users (username,password,age,gender) values("+_username+","+_password+","+_age+","+_gender+");")
+    g.conn.execute("insert into users (username,password,age,gender) values('"+_username+"','"+_password+"',"+_age+",'"+_gender+"');")
     flash("Sign Up successful! Please Sign In", category='success')
     cursor.close()
     return redirect('/SignIn')
+
 
 if __name__ == "__main__":
   import click
